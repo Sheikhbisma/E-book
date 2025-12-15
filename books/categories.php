9145fe8c0e5e46d735e3053addae9ba5cf2167ae
syedasawera27 <syedasaweranoorhussainshah27@gmail.com>
2025-12-15 23:27:14 +0500

    farah

--- books/categories.php
+++ books/categories.php
@@ -221,7 +221,7 @@ $result = mysqli_query($conn, "SELECT * FROM books");
                 let author = card.dataset.author;
                 let category = card.dataset.category;
 
-                let matchSearch = title.includes(search) || author.includes(search);
+            let matchSearch = title.includes(search) || author.includes(search);
                 let matchCategory = selectedCategories.length === 0 || selectedCategories.includes(category);
 
                 card.style.display = (matchSearch && matchCategory) ? "" : "none";
