4d9803eb5c920dfa8becf8da37fb332a9e2a8476
syedasawera27 <syedasaweranoorhussainshah27@gmail.com>
2025-12-17 01:54:48 +0500

    hashiiiiiiiiiiii

--- components/footer.php
+++ components/footer.php
@@ -58,6 +58,7 @@
                 <div class="d-flex gap-3">
                     <a href="#"><i class="fab fa-facebook fs-5"></i></a>
                     <a href="#"><i class="fab fa-instagram fs-5"></i></a>
+            
                     <a href="#"><i class="fab fa-twitter fs-5"></i></a>
                     <a href="#"><i class="fab fa-linkedin fs-5"></i></a>
                 </div>
