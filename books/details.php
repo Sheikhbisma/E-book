cbf7dabca7508d5a89b23a5508c0e06120a5a07d
waniaa <waniaabrar2007@gmail.com>
2025-12-18 15:19:43 +0500

    books doness

--- books/details.php
+++ books/details.php
@@ -9,11 +9,13 @@ if (!isset($_GET['id'])) {
     exit;
 }
 
+
 $book_id = $_GET['id'];
 
 $query = "SELECT * FROM books WHERE id = $book_id LIMIT 1";
 $result = mysqli_query($conn, $query);
 $book = mysqli_fetch_assoc($result);
+
 
 if (!$book) {
     echo "Book not found!";
@@ -25,7 +27,8 @@ if (!$book) {
 <html>
 <head>
     <title><?php echo $book['title']; ?> - Book Details</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <?php include 'inc/link.php'; ?>
+
 </head>
 
 <body class="bg-light">
