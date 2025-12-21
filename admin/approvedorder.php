4b1fa3ed93303aeabcad2132354b5f0dc1f73122
syedasawera27 <syedasaweranoorhussainshah27@gmail.com>
2025-12-21 15:51:28 +0500

    haris

--- admin/approvedorder.php
+++ admin/approvedorder.php
@@ -3,7 +3,7 @@ include '../auth/dbconnect.php';
 include 'session.php';
 $order_id = $_GET['order_id'];
 if(isset($order_id)){
-    $update = "update orders set payment_status = 'Received',order_status = 'Done' where order_id = $order_id";
+    $update = "update orders set payment_status = 'Received',order_status = 'Done' where order_id = '$order_id'";
     $execute_update = mysqli_query($conn , $update);
 if($execute_update){
     $_SESSION['approved'] = showErr("Order updated to approved" , "success");
@@ -27,10 +27,11 @@ if($execute_update){
     $_SESSION['approved'] = showErr("There is an error" , "danger");
 
 }
+
 }
 $deliver_id = $_GET['deliver_id'];
-if(isset($shipped_id)){
-    $update = "update orders set payment_status = 'Received',order_status = 'delivered' where order_id = $deliver_id";
+if(isset($deliver_id)){
+    $update = "update orders set payment_status = 'Received',order_status = 'delivered' where order_id = '$deliver_id'";
     $execute_update = mysqli_query($conn , $update);
 if($execute_update){
     $_SESSION['approved'] = showErr("Order updated to approved" , "success");
